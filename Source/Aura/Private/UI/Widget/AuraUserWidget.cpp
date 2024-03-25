0a9061319e5ffd57b97693cad7f7216dee8649d2
Joshua Villgren <joshuavillgren@gmail.com>
2024-03-26 01:45:48 +1300

    add first widget setups

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,12 @@
+// Copyright Baconeta Studios
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright Baconeta Studios
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
+
